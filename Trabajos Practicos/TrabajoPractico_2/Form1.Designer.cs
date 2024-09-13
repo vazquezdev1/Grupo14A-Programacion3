529262b96a268ef1ca1bd35096ae8659190bb885
Henry Vazquez <henry.vazquez@alumnos.frgp.utn.edu.ar>
2024-09-13 02:51:23 -0300

    Se mejoró la consulta a la DB para que traiga el precio y descripción (tanto de la marca como de la categoría)

---
+++ Trabajos Practicos/TrabajoPractico_2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TrabajoPractico_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.LblMsgBienvenida = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblIngreseUserName = new System.Windows.Forms.Label();
+            this.btnDevsBy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnInicio.Location = new System.Drawing.Point(301, 254);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(132, 52);
+            this.btnInicio.TabIndex = 3;
+            this.btnInicio.Text = "INICIAR SISTEMA";
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // LblMsgBienvenida
+            // 
+            this.LblMsgBienvenida.AutoSize = true;
+            this.LblMsgBienvenida.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMsgBienvenida.Location = new System.Drawing.Point(160, 53);
+            this.LblMsgBienvenida.Name = "LblMsgBienvenida";
+            this.LblMsgBienvenida.Size = new System.Drawing.Size(390, 33);
+            this.LblMsgBienvenida.TabIndex = 0;
+            this.LblMsgBienvenida.Text = "Bienvenido al sistema SAA";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(284, 192);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(165, 20);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // lblIngreseUserName
+            // 
+            this.lblIngreseUserName.AutoSize = true;
+            this.lblIngreseUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngreseUserName.Location = new System.Drawing.Point(107, 159);
+            this.lblIngreseUserName.Name = "lblIngreseUserName";
+            this.lblIngreseUserName.Size = new System.Drawing.Size(216, 20);
+            this.lblIngreseUserName.TabIndex = 1;
+            this.lblIngreseUserName.Text = "Ingrese el usuario de acceso:";
+            // 
+            // btnDevsBy
+            // 
+            this.btnDevsBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDevsBy.Location = new System.Drawing.Point(588, 393);
+            this.btnDevsBy.Name = "btnDevsBy";
+            this.btnDevsBy.Size = new System.Drawing.Size(131, 24);
+            this.btnDevsBy.TabIndex = 4;
+            this.btnDevsBy.Text = "Desarrollado por...";
+            this.btnDevsBy.UseVisualStyleBackColor = true;
+            this.btnDevsBy.Click += new System.EventHandler(this.btnDevsBy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(731, 429);
+            this.Controls.Add(this.btnDevsBy);
+            this.Controls.Add(this.lblIngreseUserName);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.LblMsgBienvenida);
+            this.Controls.Add(this.btnInicio);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(747, 468);
+            this.MinimumSize = new System.Drawing.Size(747, 468);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SAA - Sistema Administrador Articulos";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Label LblMsgBienvenida;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblIngreseUserName;
+        private System.Windows.Forms.Button btnDevsBy;
+    }
+}
+
