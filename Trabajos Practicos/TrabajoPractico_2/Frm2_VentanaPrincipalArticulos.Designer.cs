3b4b95c28a2cc51c80f4f8fb3fa1dd82680c938d
Henry Vazquez <henry.vazquez@alumnos.frgp.utn.edu.ar>
2024-10-01 18:55:38 -0300

    style(Frm2_VentanaPrincipalArticulos): Cambios en detalles de la interfaz gráfica de la 2da pantalla (Letra oscura y cambio de color del fondo de la fecha, etc.)

--- Trabajos Practicos/TrabajoPractico_2/Frm2_VentanaPrincipalArticulos.Designer.cs
+++ Trabajos Practicos/TrabajoPractico_2/Frm2_VentanaPrincipalArticulos.Designer.cs
@@ -102,7 +102,7 @@
             this.listarTodosLosArticulosToolStripMenuItem.Name = "listarTodosLosArticulosToolStripMenuItem";
             this.listarTodosLosArticulosToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
             this.listarTodosLosArticulosToolStripMenuItem.Size = new System.Drawing.Size(241, 22);
-            this.listarTodosLosArticulosToolStripMenuItem.Text = "&Listar todos los articulos";
+            this.listarTodosLosArticulosToolStripMenuItem.Text = "&Listar todos los artículos";
             this.listarTodosLosArticulosToolStripMenuItem.Click += new System.EventHandler(this.listarTodosLosArticulosToolStripMenuItem_Click);
             // 
             // filtrosToolStripMenuItem
@@ -110,7 +110,7 @@
             this.filtrosToolStripMenuItem.Name = "filtrosToolStripMenuItem";
             this.filtrosToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
             this.filtrosToolStripMenuItem.Size = new System.Drawing.Size(241, 22);
-            this.filtrosToolStripMenuItem.Text = "Filtros...";
+            this.filtrosToolStripMenuItem.Text = "Filtros";
             this.filtrosToolStripMenuItem.Click += new System.EventHandler(this.filtrosToolStripMenuItem_Click);
             // 
             // statusStrip1
@@ -126,9 +126,11 @@
             // 
             // tsslDatosEstadoSesion
             // 
+            this.tsslDatosEstadoSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.tsslDatosEstadoSesion.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tsslDatosEstadoSesion.Name = "tsslDatosEstadoSesion";
-            this.tsslDatosEstadoSesion.Size = new System.Drawing.Size(44, 17);
-            this.tsslDatosEstadoSesion.Text = "FECHA";
+            this.tsslDatosEstadoSesion.Size = new System.Drawing.Size(39, 17);
+            this.tsslDatosEstadoSesion.Text = "Fecha";
             // 
             // Frm2_VentanaPrincipalArticulos
             // 
@@ -138,11 +140,12 @@
             this.ClientSize = new System.Drawing.Size(722, 445);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.mnsAcciones);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.mnsAcciones;
             this.Name = "Frm2_VentanaPrincipalArticulos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Principal Articulos";
+            this.Text = "Principal";
             this.mnsAcciones.ResumeLayout(false);
             this.mnsAcciones.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
